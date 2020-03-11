e4a60e980b00741489ff47f9b36d43373f74dcff
Wouter van der Heijde <wouter.van.der.heijde@hva.nl>
2020-03-11 19:21:41 +0100

    :sparkles: able to write API fetch to local file

---
+++ scripts/write-JSON.js
@@ -0,0 +1,20 @@
+const fs = require('fs-extra')
+const dotApi = require('../docs/controllers/api/dotApi')
+const path = require('path')
+
+require("dotenv").config()
+
+dotApi.getHeroes(`/heroStats`).then(heroesData => {
+
+    let jsonContent = `${JSON.stringify(heroesData, null, 2)}`
+    console.log(jsonContent)
+    fs.mkdirp(path.join(__dirname, '..', 'data'))
+    fs.writeFile(path.join(__dirname, '..', 'data/data.json'), jsonContent, 'utf8', function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.");
+            return console.log(err);
+        }
+
+        console.log("JSON file has been saved.");
+    })
+})
