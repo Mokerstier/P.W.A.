66a9d6aedd0b9a1381cf72ef110ef061d3b54541
Mokerstier <pannekoek-wouter@hotmail.com>
2020-03-13 13:13:09 +0100

    :pencil: added procfile

--- docs/controllers/render/render.js
+++ docs/controllers/render/render.js
@@ -1,6 +1,6 @@
 const dotApi = require('../api/dotApi')
 const fs = require('fs-extra')
-// const getHero = require('../getStored/getHero')
+const getter = require('../getStored/getter')
 const recentMatches = '/recentMatches'
 
 function renderHome(req, res) {
@@ -8,18 +8,36 @@ function renderHome(req, res) {
         title: 'Home'
     })
 }
-async function renderHeroes(req, res) {
-    //const heroesData = await dotApi.getHeroes(req.path)
-    // heroes = getHero.getHero()
+async function renderHeroes(req, res) { 
     fs.readFile('data/data.json', (err, data) => {
         if (err)
             throw err;
         let heroes = JSON.parse(data);
-        res.render('pages/heroes.ejs', {
-            data: heroes.heroes,
-            title: 'Home'
-        });
+    res.render('pages/heroes.ejs', {
+        data: heroes.heroes,
+        title: 'Home'
+    })
     });
+
+}
+function renderDetail(req, res) {
+    // const heroes = getter.getHeroes()
+    // console.log(heroes)
+    fs.readFile('data/data.json', (err, data) => {
+        if (err)
+            throw err;
+        const dataHeroes = JSON.parse(data)
+        const heroes = dataHeroes.heroes
+        const id = req.params.id
+
+        const thisHero = heroes.filter(hero => hero.id == id)
+        const hero = thisHero[0]
+        res.render('pages/detail.ejs',{
+            hero: hero,
+            title: `Detail ${hero.localized_name}`
+           
+        })
+    })
 
 }
 async function renderStats(req, res) {
@@ -30,27 +48,24 @@ async function renderStats(req, res) {
         if (err)
             throw err;
         const dataHeroes = JSON.parse(data);
-        const heroes = dataHeroes.heroes 
-        
+        const heroes = dataHeroes.heroes
+
         recentMatchesData.map(match => {
-            const matchHero = heroes.filter(hero => match.hero_id === hero.id) 
-            matchHero.map(hero =>{
+            const matchHero = heroes.filter(hero => match.hero_id === hero.id)
+            matchHero.map(hero => {
                 heroNames.push(hero.localized_name)
             })
         })
         res.render('pages/myStats.ejs', {
             matches: recentMatchesData,
-            // matchHero: matchHero,
             heroName: heroNames,
             data: myStats,
             title: 'My stats'
         });
-        
-    })
-    
-}
-// function renderDetail(req, res){
-//     const heroesData = 
-// }
 
-module.exports = { renderHome, renderHeroes, renderStats }
+    })
+
+}
+
+
+module.exports = { renderDetail, renderHome, renderHeroes, renderStats }
