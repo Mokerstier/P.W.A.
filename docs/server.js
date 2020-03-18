c93b5d227a129599396029b8401b08723fe7029b
Mokerstier <pannekoek-wouter@hotmail.com>
2020-03-18 16:02:17 +0100

    :construction: working on log-in

--- docs/server.js
+++ docs/server.js
@@ -1,10 +1,24 @@
 const express = require('express')
-const bodyParser = require("body-parser");
+const session = require("express-session")
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const urlencodedParser = bodyParser.urlencoded({ extended: true })
+const cookieParser = require("cookie-parser")
+const passport = require("passport")
+const flash = require("connect-flash")
 
 const { routes } = require("./routes/routes");
 
+const user = require("./controllers/users");
+
 const port = 8000
 const app = express()
+
+mongoose.connect(uri, options);
+mongoose.connection.on("open", function(err, doc) {
+	console.log(`connection established with ${process.env.DB_NAME}`);
+	if (err) throw err;
+});
 
 app
     .set('view engine', 'ejs')
