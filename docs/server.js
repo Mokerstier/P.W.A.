dd71e24c447d20d25745260c346f9ec91fbf84cf
Mokerstier <pannekoek-wouter@hotmail.com>
2020-03-23 15:03:15 +0100

    working on servive worker

--- docs/server.js
+++ docs/server.js
@@ -5,27 +5,35 @@ const bodyParser = require("body-parser")
 const urlencodedParser = bodyParser.urlencoded({ extended: true })
 const cookieParser = require("cookie-parser")
 const passport = require("passport")
-const flash = require("connect-flash")
+
 
 const { routes } = require("./routes/routes");
 
-const user = require("./controllers/users");
+//const {user} = require("./controllers/users");
 
 const port = 8000
 const app = express()
 
-mongoose.connect(uri, options);
-mongoose.connection.on("open", function(err, doc) {
-	console.log(`connection established with ${process.env.DB_NAME}`);
-	if (err) throw err;
-});
+// mongoose.connect(uri, options);
+// mongoose.connection.on("open", function(err, doc) {
+// 	console.log(`connection established with ${process.env.DB_NAME}`);
+// 	if (err) throw err;
+// });
 
 app
+    .enable('etag')
+    .set('etag', 'strong')
     .set('view engine', 'ejs')
     .set('views', 'docs/views')
+    
+
+    .use((req, res, next) => {
+        res.header('Cache-Control', 'max-age=2592000000');
+        next()
+    })
 
     .use(express.static(__dirname + '/static'))
-
+    
     .use('/', routes)
 
 app.listen(process.env.PORT || port, () => {
