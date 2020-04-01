b47985d70d5475026576cdbaea691593dd73fa97
Mokerstier <pannekoek-wouter@hotmail.com>
2020-04-01 12:12:02 +0200

    :fire: i accidently screwed my app

--- docs/server.js
+++ docs/server.js
@@ -1,25 +1,17 @@
 const express = require('express')
-const session = require("express-session")
-const mongoose = require("mongoose")
+
 const bodyParser = require("body-parser")
 const urlencodedParser = bodyParser.urlencoded({ extended: true })
-const cookieParser = require("cookie-parser")
-const passport = require("passport")
+
 const compression = require('compression')
 require('dotenv').config()
 
 const { routes } = require("./routes/routes");
 
-//const {user} = require("./controllers/users");
 
 const port = 8000
 const app = express()
 
-// mongoose.connect(uri, options);
-// mongoose.connection.on("open", function(err, doc) {
-// 	console.log(`connection established with ${process.env.DB_NAME}`);
-// 	if (err) throw err;
-// });
 
 app
     .enable('etag')
